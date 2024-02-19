8551e6f6b2b370aeb1fef260fb9a224298475cba
Estevan <egonz94@gmail.com>
2024-02-19 15:59:45 -0600

    Added first structuring. Red and Blue

---
+++ src/components/Canvas.js
@@ -0,0 +1,11 @@
+function Canvas() {
+    return (
+        <div className="first">
+            <div className="second">
+    
+            </div>
+        </div>
+    )
+}
+
+export default Canvas;
